b16e94a33458bd7ca44c510fc686294b80d01f49
arey1124 <arihant0996@gmail.com>
2023-05-22 20:01:47 -0300

    Added test case mapping for Ingredients

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,2 +1,48 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.gen5.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 public class IngredientTest {
+
+    static Ingredient ingredient;
+
+    @BeforeAll
+    public static void SetupBeforeAll() {
+        ingredient = mock(Ingredient.class);
+
+        // Set up the behavior of the mock object
+        when(ingredient.getName()).thenReturn("Vodka");
+        when(ingredient.getId()).thenReturn("1");
+        when(ingredient.getType()).thenReturn("Vodka");
+        when(ingredient.getDescription()).thenReturn("Vodka is a distilled beverage composed primarily of water and ethanol.");
+        when(ingredient.getHasAlcohol()).thenReturn("Yes");
+        when(ingredient.getAlocholByVolume()).thenReturn("40");
+    }
+
+    @Test
+    public void testIngredientNameAndIdGetter() {
+
+    }
+
+    @Test
+    public void testIngredientTypeGetterAndSetter() {
+
+    }
+
+    @Test
+    public void testIngredientDescriptionGetterAndSetter() {
+
+    }
+
+    @Test
+    public void testIngredientHasAlcoholGetterAndSetter() {
+
+    }
+
+    @Test
+    public void testIngredientAlcoholByVolumeGetterAndSetter() {
+
+    }
 }

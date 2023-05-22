e952a05060c78f325a29e50aba9463819cddea00
arey1124 <arihant0996@gmail.com>
2023-05-22 20:14:16 -0300

    Added JSONParser tests structures following TDD approach

--- src/test/java/JSONParserTest.java
+++ src/test/java/JSONParserTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Test;
+
 public class JSONParserTest {
+
+    @Test
+    public static void testParseCocktailData() {
+
+    }
+
+    @Test
+    public static void testParseCocktails() {
+
+    }
+
+    @Test
+    public static void testParseIngredientData() {
+
+    }
 }

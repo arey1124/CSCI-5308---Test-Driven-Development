0d364de538da540e8cc2fe28c07bd09f4cb43481
arey1124 <arihant0996@gmail.com>
2023-05-22 21:15:47 -0300

    Added JSONParser tests and defined logic inside JSONParser class

--- src/main/java/JSONParser.java
+++ src/main/java/JSONParser.java
@@ -1,4 +1,234 @@
+import enums.AlcoholicFilter;
+import enums.Category;
+import enums.GlassType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JSONParser {
+    public static Cocktail parseCocktailData(String data) {
+        try {
+            String cocktailName = parseName(data);
+            String cocktailId = parseId(data);
+            if(cocktailId.isEmpty() || cocktailName.isEmpty()) {
+                return null;
+            }
+            Cocktail cocktail = new Cocktail(cocktailName, cocktailId);
+            cocktail.setCategory(parseCategory(data));
+            cocktail.setIsAlcoholic(parseAlcoholicFilter(data));
+            cocktail.setGlassType(parseGlassType(data));
+            cocktail.setInstructions(parseInstructions(data));
+            cocktail.setThumbnail(parseThumbnail(data));
 
+            return cocktail;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static ArrayList<Cocktail> parseCocktails (String data) {
+        ArrayList<Cocktail> cocktails = new ArrayList<Cocktail>();
+
+        String regex = "\"strDrink\":\\s*\"([^\"]+)\",\\s*\"strDrinkThumb\":\\s*\"([^\"]+)\",\\s*\"idDrink\":\\s*\"([^\"]+)\"";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(data);
+
+        while (matcher.find()) {
+            String strDrink = matcher.group(1);
+            String idDrink = matcher.group(3);
+            Cocktail cocktail = new Cocktail(strDrink, idDrink);
+            cocktails.add(cocktail);
+        }
+
+        return cocktails;
+    }
+
+    private static String parseId(String data) {
+        String pattern = "\"idDrink\":\"(\\d+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String id = match.group(1);
+            return id;
+        }
+        return null;
+    }
+
+    private static String parseName(String data) {
+        String pattern = "\"strDrink\":\"([^\"]+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String name = match.group(1);
+            return name;
+        }
+        return null;
+    }
+
+    private static Category parseCategory(String data) {
+        String pattern = "\"strCategory\":\"([^\"]+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String category = match.group(1).replace("\\", "");;
+            return Category.fromString(category);
+        }
+        return null;
+    }
+
+    private static AlcoholicFilter parseAlcoholicFilter(String data) {
+        String pattern = "\"strAlcoholic\":\"([^\"]+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String isAlcoholic = match.group(1).replace("\\", "");;
+            return AlcoholicFilter.fromString(isAlcoholic);
+        }
+        return null;
+    }
+
+    private static GlassType parseGlassType(String data) {
+        String pattern = "\"strGlass\":\"([^\"]+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String glassType = match.group(1).replace("\\", "");;
+            return GlassType.fromGlassName(glassType);
+        }
+        return null;
+    }
+
+    private static String parseInstructions(String data) {
+        String pattern = "\"strInstructions\":\"([^\"]+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String instructions = match.group(1);
+            return instructions;
+        }
+        return null;
+    }
+
+    private static String parseThumbnail(String data) {
+        String pattern = "\"strDrinkThumb\":\"([^\"]+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String thumbnail = match.group(1);
+            return thumbnail.replace("\\/","/");
+        }
+        return null;
+    }
+
+    public static Ingredient parseIngredientData (String data) {
+        try {
+            String ingredientName = parseIngredientName(data);
+            String ingredientId = parseIngredientId(data);
+            if(ingredientName.isEmpty() || ingredientId.isEmpty()) {
+                return null;
+            }
+            Ingredient ingredient = new Ingredient(ingredientName, ingredientId);
+            ingredient.setDescription(parseIngredientDescription(data));
+            ingredient.setType(parseIngredientType(data));
+            ingredient.setHasAlcohol(parseIngredientHasAlcohol(data));
+            ingredient.setAlocholByVolume(parseIngredientAlcoholByVolume(data));
+            return ingredient;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private static String parseIngredientId(String data) {
+        String pattern = "\"idIngredient\":\"([^\"]+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String thumbnail = match.group(1);
+            return thumbnail;
+        }
+        return null;
+    }
+
+    private static String parseIngredientName(String data) {
+        String pattern = "\"strIngredient\":\"([^\"]+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String thumbnail = match.group(1);
+            return thumbnail;
+        }
+        return null;
+    }
+
+    private static String parseIngredientDescription(String data) {
+        String pattern = "\"strDescription\":\"([^\"]+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String thumbnail = match.group(1);
+            return thumbnail;
+        }
+        return null;
+    }
+
+    private static String parseIngredientType(String data) {
+        String pattern = "\"strType\":\"([^\"]+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String thumbnail = match.group(1);
+            return thumbnail;
+        }
+        return null;
+    }
+
+    private static String parseIngredientHasAlcohol(String data) {
+        String pattern = "\"strAlcohol\":\"([^\"]+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String thumbnail = match.group(1);
+            return thumbnail;
+        }
+        return null;
+    }
+
+    private static String parseIngredientAlcoholByVolume(String data) {
+        String pattern = "\"strABV\":\"([^\"]+)\"";
+        // Create a Pattern object
+        Pattern pat = Pattern.compile(pattern);
+        // Create a Matcher object
+        Matcher match = pat.matcher(data);
+        if (match.find()) {
+            String thumbnail = match.group(1);
+            return thumbnail;
+        }
+        return null;
+    }
 }
--- src/test/java/JSONParserTest.java
+++ src/test/java/JSONParserTest.java
@@ -1,19 +1,130 @@
+import enums.AlcoholicFilter;
+import enums.Category;
+import enums.GlassType;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.*;
 
 public class JSONParserTest {
 
-    @Test
-    public static void testParseCocktailData() {
+    @Spy
+    private static JSONParser jsonParser;
 
+    @BeforeAll
+    public static void SetupBeforeAll() {
+        // Set up the behavior of the mock object
+        jsonParser = mock(JSONParser.class);
     }
 
     @Test
-    public static void testParseCocktails() {
+    public void testParseCocktailData_ValidData_ReturnsCocktail() {
+        // Set up the data for the mock object
+        String data = "{\"drinks\":[{\"idDrink\":\"17060\",\"strDrink\":\"24k nightmare\",\"strDrinkAlternate\":null,\"strTags\":null,\"strVideo\":null,\"strCategory\":\"Shot\",\"strIBA\":null,\"strAlcoholic\":\"Alcoholic\",\"strGlass\":\"Shot glass\",\"strInstructions\":\"Add over ice,shake and pour.\",\"strInstructionsES\":null,\"strInstructionsDE\":\"\\u00dcber das Eis geben, sch\\u00fctteln und eingiessen.\",\"strInstructionsFR\":null,\"strInstructionsIT\":\"Aggiungere gli ingredienti dopo aver versato un po\\u2019 di ghiaccio, agitare e versare.\",\"strInstructionsZH-HANS\":null,\"strInstructionsZH-HANT\":null,\"strDrinkThumb\":\"https:\\/\\/www.thecocktaildb.com\\/images\\/media\\/drink\\/yyrwty1468877498.jpg\",\"strIngredient1\":\"Goldschlager\",\"strIngredient2\":\"J\\u00e4germeister\",\"strIngredient3\":\"Rumple Minze\",\"strIngredient4\":\"151 proof rum\",\"strIngredient5\":null,\"strIngredient6\":null,\"strIngredient7\":null,\"strIngredient8\":null,\"strIngredient9\":null,\"strIngredient10\":null,\"strIngredient11\":null,\"strIngredient12\":null,\"strIngredient13\":null,\"strIngredient14\":null,\"strIngredient15\":null,\"strMeasure1\":\"1\\/2 oz \",\"strMeasure2\":\"1\\/2 oz \",\"strMeasure3\":\"1\\/2 oz \",\"strMeasure4\":\"1\\/2 oz Bacardi \",\"strMeasure5\":null,\"strMeasure6\":null,\"strMeasure7\":null,\"strMeasure8\":null,\"strMeasure9\":null,\"strMeasure10\":null,\"strMeasure11\":null,\"strMeasure12\":null,\"strMeasure13\":null,\"strMeasure14\":null,\"strMeasure15\":null,\"strImageSource\":null,\"strImageAttribution\":null,\"strCreativeCommonsConfirmed\":\"No\",\"dateModified\":\"2016-07-18 22:31:38\"}]}";
+        Cocktail expectedCocktail = new Cocktail("24k nightmare", "17060");
 
+        // Retrive the result of parsed cocktail data
+        Cocktail result = jsonParser.parseCocktailData(data);
+
+        // Assert values to see if the data is correct
+        assertEquals(expectedCocktail.getName(), result.getName());
+        assertEquals(expectedCocktail.getId(), result.getId());
+        assertEquals(Category.SHOT, result.getCategory());
+        assertEquals(AlcoholicFilter.ALCOHOLIC, result.getIsAlcoholic());
+        assertEquals(GlassType.SHOT_GLASS, result.getGlassType());
+        assertEquals("Add over ice,shake and pour.", result.getInstructions());
+        assertEquals("https://www.thecocktaildb.com/images/media/drink/yyrwty1468877498.jpg", result.getThumbnail());
     }
 
     @Test
-    public static void testParseIngredientData() {
+    public void testParseCocktailData_InvalidData_ReturnsNull() {
+        String data = "{\"drinks\":[{\"idDrink\":\"17060\",\"strDrink\":\"\",\"strDrinkAlternate\":null,\"strTags\":null,\"strVideo\":null,\"strCategory\":\"Shot\",\"strIBA\":null,\"strAlcoholic\":\"Alcoholic\",\"strGlass\":\"Shot glass\",\"strInstructions\":\"Add over ice,shake and pour.\",\"strInstructionsES\":null,\"strInstructionsDE\":\"\\u00dcber das Eis geben, sch\\u00fctteln und eingiessen.\",\"strInstructionsFR\":null,\"strInstructionsIT\":\"Aggiungere gli ingredienti dopo aver versato un po\\u2019 di ghiaccio, agitare e versare.\",\"strInstructionsZH-HANS\":null,\"strInstructionsZH-HANT\":null,\"strDrinkThumb\":\"https:\\/\\/www.thecocktaildb.com\\/images\\/media\\/drink\\/yyrwty1468877498.jpg\",\"strIngredient1\":\"Goldschlager\",\"strIngredient2\":\"J\\u00e4germeister\",\"strIngredient3\":\"Rumple Minze\",\"strIngredient4\":\"151 proof rum\",\"strIngredient5\":null,\"strIngredient6\":null,\"strIngredient7\":null,\"strIngredient8\":null,\"strIngredient9\":null,\"strIngredient10\":null,\"strIngredient11\":null,\"strIngredient12\":null,\"strIngredient13\":null,\"strIngredient14\":null,\"strIngredient15\":null,\"strMeasure1\":\"1\\/2 oz \",\"strMeasure2\":\"1\\/2 oz \",\"strMeasure3\":\"1\\/2 oz \",\"strMeasure4\":\"1\\/2 oz Bacardi \",\"strMeasure5\":null,\"strMeasure6\":null,\"strMeasure7\":null,\"strMeasure8\":null,\"strMeasure9\":null,\"strMeasure10\":null,\"strMeasure11\":null,\"strMeasure12\":null,\"strMeasure13\":null,\"strMeasure14\":null,\"strMeasure15\":null,\"strImageSource\":null,\"strImageAttribution\":null,\"strCreativeCommonsConfirmed\":\"No\",\"dateModified\":\"2016-07-18 22:31:38\"}]}";
+        Cocktail expectedCocktail = new Cocktail("24k nightmare", "17060");
 
+        // Retrive the result of parsed cocktail data
+        Cocktail result = jsonParser.parseCocktailData(data);
+
+        // Assert
+        assertNull(result);
+
+        // Verify that no other methods were called
+        verifyNoMoreInteractions(jsonParser);
+    }
+
+    @Test
+    public void testParseCocktails_ValidData_ReturnsCocktail() {
+        // Set up the data for the mock object
+        String data = "{\"drinks\":[{\"strDrink\":\"3-Mile Long Island Iced Tea\",\"strDrinkThumb\":\"https:\\/\\/www.thecocktaildb.com\\/images\\/media\\/drink\\/rrtssw1472668972.jpg\",\"idDrink\":\"15300\"},{\"strDrink\":\"69 Special\",\"strDrinkThumb\":\"https:\\/\\/www.thecocktaildb.com\\/images\\/media\\/drink\\/vqyxqx1472669095.jpg\",\"idDrink\":\"13940\"}]}";
+        ArrayList<Cocktail> expected = new ArrayList<>();
+        expected.add(new Cocktail("3-Mile Long Island Iced Tea","15300"));
+        expected.add(new Cocktail("69 Special", "13940"));
+
+        // Retrive the result of parsed cocktail data
+        ArrayList<Cocktail> actual = jsonParser.parseCocktails(data);
+
+        // Assert to see if the array size is correct
+        assertEquals(expected.size(), actual.size());
+
+        for(int i = 0; i < expected.size(); i++) {
+            // Assert values to see if the data is correct
+            assertEquals(expected.get(i).getName(), actual.get(i).getName());
+            assertEquals(expected.get(i).getId(), actual.get(i).getId());
+        }
+    }
+
+    @Test
+    public void testParseCocktails_InvalidData_ReturnsEmpty() {
+        // Set up the data for the mock object
+        String data = "{\"drinks\":[]}";
+        ArrayList<Cocktail> expected = new ArrayList<>();
+
+        // Retrive the result of parsed cocktail data
+        ArrayList<Cocktail> actual = jsonParser.parseCocktails(data);
+
+        // Assert to see if the array size is correct
+        assertEquals(expected.size(), actual.size());
+
+        // Assert to see if the returned list is empty
+        assertEquals(0, actual.size());
+    }
+
+    @Test
+    public void testParseIngredientData_ValidData_ReturnsIngredient() {
+        // Set up the data for the mock object
+        String data = "{\"ingredients\":[{\"idIngredient\":\"552\",\"strIngredient\":\"Elderflower cordial\",\"strDescription\":\"Elderflower cordial is a soft drink made largely from a refined sugar and water solution and uses the flowers of the European elderberry. Historically the cordial has been popular in North Western Europe where it has a strong Victorian heritage.\",\"strType\":\"Cordial\",\"strAlcohol\":\"No\",\"strABV\":\"50\"}]}";
+        Ingredient expected = new Ingredient("Elderflower cordial", "552");
+
+        // Retrive the result of parsed ingredient data
+        Ingredient actual = jsonParser.parseIngredientData(data);
+
+        // Assert values to see if the data is correct
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals("Cordial", actual.getType());
+        assertEquals("No", actual.getHasAlcohol());
+        assertEquals("Elderflower cordial is a soft drink made largely from a refined sugar and water solution and uses the flowers of the European elderberry. Historically the cordial has been popular in North Western Europe where it has a strong Victorian heritage.",
+                actual.getDescription());
+        assertEquals("50", actual.getAlocholByVolume());
+    }
+
+    @Test
+    public void testParseIngredientData_InvalidData_ReturnsNull() {
+        // Set up the data for the mock object
+        String data = "{\"ingredients\":[{\"idIngredient\":\"552\",\"strIngredient\":\"\",\"strDescription\":\"Elderflower cordial is a soft drink made largely from a refined sugar and water solution and uses the flowers of the European elderberry. Historically the cordial has been popular in North Western Europe where it has a strong Victorian heritage.\",\"strType\":\"Cordial\",\"strAlcohol\":\"No\",\"strABV\":\"50\"}]}";
+        Ingredient expected = new Ingredient("Elderflower cordial", "552");
+
+        // Retrive the result of parsed ingredient data
+        Ingredient actual = jsonParser.parseIngredientData(data);
+
+        // Assert
+        assertNull(actual);
+
+        // Verify that no other methods were called
+        verifyNoMoreInteractions(jsonParser);
     }
 }
